763fe59c44c54a8fca2412c8c558e1741d66806a
Carol Navarro Pereira <carol.navarro.pereira@oracle.com>
2025-09-08 12:47:38 +0200

    draft 1

--- packages/node-cli/src/services/DevAssistProxyService.js
+++ packages/node-cli/src/services/DevAssistProxyService.js
@@ -3,52 +3,75 @@
  ** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
  */
 'use strict';
+/** http libraries */
 const http = require('node:http');
 const https = require('node:https');
-const EventEmitter = require('events');
 
+/** Authentication methods */
 const {
 	getAuthIds,
 	checkIfReauthorizationIsNeeded,
 	forceRefreshAuthorization,
 } = require('../utils/AuthenticationUtils');
+const {
+	AUTHORIZATION_PROPERTIES_KEYS,
+} = require('../ApplicationConstants');
+
+/** Message literal service method */
 const NodeTranslationService = require('./NodeTranslationService');
 const {
 	DEV_ASSIST_PROXY_SERVICE,
-	ANSWERS_VALIDATION_MESSAGES,
-	COMMAND_REFRESH_AUTHORIZATION,
 } = require('./TranslationKeys');
-const { AUTHORIZATION_PROPERTIES_KEYS } = require('../ApplicationConstants');
+
 const SdkOperationResult = require('../utils/SdkOperationResult');
+const { ServerResponse, IncomingMessage } = require('node:http');
 
 const LOCAL_HOSTNAME = '127.0.0.1';
 const UNAUTHORIZED_RESPONSE = 401;
 const INTERNAL_SERVER_ERROR_RESPONSE = 500;
 
-module.exports = class DevAssistProxyService extends EventEmitter {
+/** Target server port */
+const TARGET_SERVER_PORT = 443;
+
+module.exports = class DevAssistProxyService {
 
 	constructor(sdkPath, executionEnvironmentContext) {
-		super();
 		this._sdkPath = sdkPath;
 		this._executionEnvironmentContext = executionEnvironmentContext;
-		this._proxy = http.createServer();
-		this._name = 'DevAssistProxyService'; //TODO move to parameter
-		this._proxyReq = undefined; //TODO see if I can assign later
-		this._accessToken =  undefined;
+		this._accessToken = undefined;
+		this._targetHost = undefined;
+		this._proxy = undefined;
+		this._authId = undefined;
 	}
 
-	_buildOptions(hostName, accessToken, req) {
-		const authorization = 'Bearerr ' + accessToken;
+	async _retrieveAccessToken() {
+		const authIDActionResult = await getAuthIds(this._sdkPath);
+
+		if (!authIDActionResult.isSuccess()) {
+			throw authIDActionResult.errorMessages;
+		}
+
+		if (!authIDActionResult.data.hasOwnProperty(this._authId)) {
+			throw NodeTranslationService.getMessage(DEV_ASSIST_PROXY_SERVICE.NOT_EXISTING_AUTH_ID, this._authId);
+		}
+		return {
+			accessToken: authIDActionResult.data[this._authId].token.accessToken,
+			hostName: authIDActionResult.data[this._authId].hostInfo.hostName,
+		};
+	}
+
+	_buildOptions(clineRequest) {
+		const authorization = 'Bearer ' + this._accessToken;
 		const options = {
-			hostname: hostName,
-			port: 443,
-			path: req.url,
-			method: req.method,
-			headers: { ...req.headers, authorization },
+			hostname: this._targetHost,
+			port: TARGET_SERVER_PORT,
+			path: clineRequest.url,
+			method: clineRequest.method,
+			headers: { ...clineRequest.headers, authorization },
 		};
 
 		// Add agent for insecure connections when connecting to runboxes
-		if (hostName && hostName.includes('vm.eng')) {
+		if (this._targetHost && this._targetHost.includes('vm.eng')) {
 			console.log('Disabling reject unauthorized');
 			options.agent = new https.Agent({
 				rejectUnauthorized: false,
@@ -56,104 +79,11 @@ module.exports = class DevAssistProxyService extends EventEmitter {
 			options.rejectUnauthorized = false;
 		}
 
-
 		console.log('Target: ' + options.hostname);
 		console.log('Path: ' + options.path);
 		console.log('Authorization: ' + (options.headers?.authorization?.substring(0, 20) + '...'));
-
 		return options;
 	}
-
-	async start(authId, localPort) {
-
-		//Parameters validation
-		if (!authId) {
-			throw NodeTranslationService.getMessage(DEV_ASSIST_PROXY_SERVICE.MISSING_AUTH_ID);
-		}
-
-		if (!localPort) {
-			throw NodeTranslationService.getMessage(DEV_ASSIST_PROXY_SERVICE.MISSING_PORT);
-		}
-
-		if (isNaN(localPort)) {
-			throw NodeTranslationService.getMessage(DEV_ASSIST_PROXY_SERVICE.PORT_MUST_BE_NUMBER);
-		}
-
-		let { accessToken, hostName } = await this._retrieveAccessToken(authId);
-		this._accessToken = accessToken;
-
-		//TODO check a server is running or not
-
-
-		this._proxy.addListener('request', async (req, res) => {
-			console.log(`${req.method} ${req.url}`);
-			//we need the newAccessToken
-			this._createProxyReq(authId, hostName, this._accessToken, req, res, 0);
-			console.log('After add listener');
-		});
-
-		this._proxy.listen(localPort, LOCAL_HOSTNAME, () => {
-			//TODO testing later	this.emit('start', this._name);
-			const localURL = `http://${LOCAL_HOSTNAME}:${localPort}`;
-			console.log(`SuiteCloud Proxy server listening on ${localURL}`);
-			console.log(`Set Cline Base URL to: ${localURL}/api/internal/devassist`);
-			console.log('SuiteCloud Proxy running.');
-			console.log(`Configure Cline Base URL to: ${localURL}/api/internal/devassist`);
-			console.log(`SuiteCloud Proxy server listening on ${localURL}`);
-			console.log(`SuiteCloud Proxy is using the ${authId} authID`);
-			console.log(`Configure Cline Base URL to: ${localURL}/api/internal/devassist`);
-		});
-	}
-
-	async stop() {
-		if (this._proxy.listening) {
-			this._proxy.close(() => {
-				console.log('SuiteCloud Proxy stopped.');
-				console.log('SuiteCloud Proxy stopped.');
-				console.log('SuiteCloud Proxy stopped.');
-				return new Promise((resolve) => {
-				});
-			});
-		}
-	}
-
-	_createProxyReq(authId, hostName, accessToken, clineRequest, clineResponse, numRetries) {
-		let options = this._buildOptions(hostName, accessToken, clineRequest);
-		const authenticatedRequest = https.request(options, async (serverResponse) => {
-			console.log(`Proxy response: ${serverResponse.statusCode}`);
-
-			if (serverResponse.statusCode === UNAUTHORIZED_RESPONSE && numRetries === 0) {
-				try {
-					const newAccessToken = await this._forceRefreshAuth(authId);
-					console.log('New auth token' + newAccessToken);
-					this._accessToken = newAccessToken;
-				//	clineRequest.unpipe();
-					authenticatedRequest.abort();
-					authenticatedRequest.destroy();
-
-					return this._createProxyReq(authId, hostName, newAccessToken, clineRequest, clineResponse, 1);
-				} catch (err) {
-					clineResponse.writeHead(UNAUTHORIZED_RESPONSE, serverResponse.headers);
-					serverResponse.pipe(clineResponse, { end: true });
-					console.error(` error when running foreRefreshAuth: ${err}`);
-				}
-			} //end if
-			clineResponse.writeHead(serverResponse.statusCode || INTERNAL_SERVER_ERROR_RESPONSE, serverResponse.headers);
-			serverResponse.pipe(clineResponse, { end: true });
-		});
-
-		authenticatedRequest.on('error', (err) => {
-			console.error('Proxy request error:', err);
-			clineResponse.writeHead(INTERNAL_SERVER_ERROR_RESPONSE);
-			clineResponse.end('SuiteCloud Proxy error: ' + err.message);
-		});
-
-		//if (numRetries === 0) {
-			clineRequest.pipe(authenticatedRequest, { end: true });
-		//}
-
-	}
-
 
 	async _forceRefreshAuth(authId) {
 		let inspectAuthOperationResult = await checkIfReauthorizationIsNeeded(authId, this._sdkPath, this._executionEnvironmentContext);
@@ -163,8 +93,7 @@ module.exports = class DevAssistProxyService extends EventEmitter {
 
 		let inspectAuthData = inspectAuthOperationResult.data;
 		if (inspectAuthData[AUTHORIZATION_PROPERTIES_KEYS.NEEDS_REAUTHORIZATION]) {
-			//TODO move to messages
-			throw new Error('Need to reauthenticate');
+			throw NodeTranslationService.getMessage(DEV_ASSIST_PROXY_SERVICE.NEED_TO_REAUTHENTICATE);
 		} else {
 			//force refresh
 			let result = await forceRefreshAuthorization(authId, this._sdkPath, this._executionEnvironmentContext);
@@ -172,24 +101,98 @@ module.exports = class DevAssistProxyService extends EventEmitter {
 				//TODO ensure there is no error here
 				throw result.errorMessages[0];
 			}
-			return result.data.accessToken;
+			this._accessToken = result.data.accessToken;
 		}
 	}
 
+	_handleProxyError(res, err) {
+		console.error('Proxy request error:', err);
+		res.writeHead(500);
+		res.end('SuiteCloud Proxy error: ' + err.message);
+	}
 
-	async _retrieveAccessToken(authId) {
-		const authIDActionResult = await getAuthIds(this._sdkPath);
+	_handleProxyResponse(res, req, options, retryCount, body, proxyRes) {
+		console.log(`Proxy response ${retryCount}: ${proxyRes.statusCode}`);
 
-		if (!authIDActionResult.isSuccess()) {
-			throw authIDActionResult.errorMessages;
+		if (proxyRes.statusCode === UNAUTHORIZED_RESPONSE && retryCount < 1) {
+			proxyRes.resume(); // Consume the original response to prevent hanging
+			this._forceRefreshAuth(this._authId).then(() => {
+				// Retry the request with updated token
+				const newAuthorization = 'Bearer ' + this._accessToken;
+				const newOptions = {
+					...options,
+					headers: { ...options.headers, authorization: newAuthorization },
+				};
+				const newProxyReq = https.request(newOptions, this._handleProxyResponse.bind(this, res, req, options, retryCount + 1, body));
+				newProxyReq.on('error', this._handleProxyError.bind(this, res));
+				newProxyReq.write(body);
+				newProxyReq.end();
+			});
+		} else {
+			res.writeHead(proxyRes.statusCode || INTERNAL_SERVER_ERROR_RESPONSE, proxyRes.headers);
+			proxyRes.pipe(res, { end: true });
+		}
+	}
+
+	async start(authId, localProxyPort) {
+
+		//Parameters validation
+		if (!authId) {
+			throw NodeTranslationService.getMessage(DEV_ASSIST_PROXY_SERVICE.MISSING_AUTH_ID);
+		} else {
+			this._authId = authId;
 		}
 
-		if (!authIDActionResult.data.hasOwnProperty(authId)) {
-			throw NodeTranslationService.getMessage(DEV_ASSIST_PROXY_SERVICE.NOT_EXISTING_AUTH_ID, authId);
+		if (!localProxyPort) {
+			throw NodeTranslationService.getMessage(DEV_ASSIST_PROXY_SERVICE.MISSING_PORT);
 		}
-		return {
-			accessToken: authIDActionResult.data[authId].token.accessToken,
-			hostName: authIDActionResult.data[authId].hostInfo.hostName,
-		};
+
+		if (isNaN(localProxyPort)) {
+			throw NodeTranslationService.getMessage(DEV_ASSIST_PROXY_SERVICE.PORT_MUST_BE_NUMBER);
+		}
+
+		let { accessToken, hostName } = await this._retrieveAccessToken();
+		this._targetHost = hostName;
+		this._accessToken = accessToken;
+
+		this._proxy = http.createServer();
+
+		this._proxy.addListener('request', (req, res) => {
+			console.log(`${req.method} ${req.url}`);
+
+			const options = this._buildOptions(req);
+
+			const bodyChunks = [];
+			req.on('data', (chunk) => bodyChunks.push(chunk));
+			req.on('end', () => {
+				const body = Buffer.concat(bodyChunks);
+				const proxyReq = https.request(options, this._handleProxyResponse.bind(this, res, req, options, 0, body));
+				proxyReq.on('error', this._handleProxyError.bind(this, res));
+				proxyReq.write(body);
+				proxyReq.end();
+			});
+		});
+
+		this._proxy.listen(localProxyPort, LOCAL_HOSTNAME, () => {
+			const localURL = `http://${LOCAL_HOSTNAME}:${localProxyPort}`;
+			console.log(`SuiteCloud Proxy server listening on ${localURL}`);
+			console.log(`Set Cline Base URL to: ${localURL}/api/internal/devassist`);
+			console.log('SuiteCloud Proxy running.');
+			console.log(`Configure Cline Base URL to: ${localURL}/api/internal/devassist`);
+			console.log(`SuiteCloud Proxy server listening on ${localURL}`);
+			console.log(`SuiteCloud Proxy is using the ${authId} authID`);
+			console.log(`Configure Cline Base URL to: ${localURL}/api/internal/devassist`);
+		});
+
+	}
+
+	async stop(authId, localProxyPort) {
+		//TODO to develop
 	}
 };
+
+
+
+
+
+
