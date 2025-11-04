51efa53d5c857c54ecfa4be10fc25ceece1fc8f4
Jorge Borras i Duarte <jorge.borras.i.duarte@oracle.com>
2025-11-04 17:31:29 +0100

    More stuff

--- packages/vscode-extension/src/webviews/FeedbackFormWebview.ts
+++ packages/vscode-extension/src/webviews/FeedbackFormWebview.ts
@@ -12,13 +12,22 @@ const translationService = new VSTranslationService();
 const MEDIA_DIR = 'resources/media'
 const WEBVIEW_FILE_NAMES = {
 	FEEDBACK_FORM : {
-		HTML : "FeedbackForm.html",
-		CSS : "FeedbackForm.css"
-	}
+		HTML : 'FeedbackForm.html',
+		CSS : 'FeedbackForm.css'
+	},
+	SUBMITTING_HTML : 'FeedbackFormSubmitting.html',
+	SUCCESS_HTML : 'FeedbackFormSucess.html',
+	FAILURE_HTML : 'FeedbackFormFailure.html',
+}
+
+const WEBVIEW_EVENTS = {
+	CLOSE : "CLOSE_WEBVIEW",
+	SUBMIT_FEEDBACK : "SUBMIT_FEEDBACK",
+	OPEN_NEW_FEEDBACK_FORM : "OPEN_NEW_FEEDBACK_FORM"
 }
 
 let feedbackFormPanel: vscode.WebviewPanel | undefined;
-
+let extensionMediaPath : string;
 export const openDevAssistFeedbackForm = (context: vscode.ExtensionContext) => {
 
 	// if one FeedbackForm is already open, reveal it instead of creating a new one
@@ -27,24 +36,27 @@ export const openDevAssistFeedbackForm = (context: vscode.ExtensionContext) => {
 		return;
 	}
 
+	extensionMediaPath = context.extensionPath;
 	feedbackFormPanel = vscode.window.createWebviewPanel(
-		'devassistfeedbackform', // viewtype (used internally by vscode)
-		translationService.getMessage(DEVASSIST_SERVICE.FEEDBACK_FORM.WEBVIEW_TITLE),
+		'devassistfeedbackform',
+		'SuiteCloud Developer Assistant Feedback',
 		vscode.ViewColumn.One,
 		{
 			enableScripts: true,
 			localResourceRoots: [
-				vscode.Uri.file(path.join(context.extensionPath, MEDIA_DIR)),
+				vscode.Uri.file(path.join(extensionMediaPath, MEDIA_DIR)),
 			],
 		},
 	);
 
 	// Read HTML and inject the correct webview resource URIs for the CSS file
-    const feedbackFormHTMLFilePath = path.join(context.extensionPath, MEDIA_DIR, WEBVIEW_FILE_NAMES.FEEDBACK_FORM.HTML);
-    const feedbackFormCSSFilePath = path.join(context.extensionPath, MEDIA_DIR, WEBVIEW_FILE_NAMES.FEEDBACK_FORM.CSS);
+	const fullMediaPath = path.join(extensionMediaPath, MEDIA_DIR);
+    const feedbackFormHTMLFilePath = path.join(fullMediaPath, WEBVIEW_FILE_NAMES.FEEDBACK_FORM.HTML);
+    const feedbackFormCSSFilePath = path.join(fullMediaPath, WEBVIEW_FILE_NAMES.FEEDBACK_FORM.CSS);
 	feedbackFormPanel.webview.html = getWebviewHTMLContent(feedbackFormHTMLFilePath, feedbackFormCSSFilePath.toString());
 
-	// Clean up the reference when the panel is closed
+
+	// Clean up the reference when the WebviewPanel is closed
 	feedbackFormPanel.onDidDispose(
 		() => {
 			feedbackFormPanel = undefined;
@@ -55,35 +67,7 @@ export const openDevAssistFeedbackForm = (context: vscode.ExtensionContext) => {
 
 	// Handle messages sent from the webview
 	feedbackFormPanel.webview.onDidReceiveMessage(
-		async (webviewMessage) => {
-			switch (webviewMessage.type) {
-				case 'submit':
-					console.log(webviewMessage);
-					feedbackFormPanel?.webview.postMessage({ type: 'status', value: 'success' });
-
-					const currentProxySettings = getDevAssistCurrentSettings();
-					try {
-						const response = await fetch(`http://127.0.0.1:$${currentProxySettings.localPort}/api/internal/devassist`, {
-							method: 'POST',
-							headers: { 'Content-Type': 'application/json' },
-							body: webviewMessage.data
-						});
-						if (response.ok) {
-							feedbackFormPanel?.webview.postMessage({ type: 'status', value: 'success' });
-						} else {
-							feedbackFormPanel?.webview.postMessage({ type: 'status', value: 'error' });
-						}
-					} catch (e) {
-						feedbackFormPanel?.webview.postMessage({ type: 'status', value: 'error' });
-					}
-					// Handle/store feedback as needed
-					// feedbackFormPanel?.dispose();
-					break;
-				case 'cancel':
-					feedbackFormPanel?.dispose();
-					break;
-			}
-		},
+		(webviewMessage) => handleWebviewMessage(webviewMessage),
 		undefined,
 		context.subscriptions,
 	);
@@ -111,6 +95,39 @@ export const debugCall = () => {
 
 // Thank you for your feedback! You can close this window\n [Close Window, write another feedback BUTTON]
 // Woah! Something went wrong when submitting your feedback.\n Please try again later
+
+const handleWebviewMessage = async (webviewMessage : any) : Promise<void> => {
+	switch (webviewMessage.type) {
+
+		case WEBVIEW_EVENTS.SUBMIT_FEEDBACK:
+			console.log(webviewMessage);
+			feedbackFormPanel?.webview.postMessage({ type: 'status', value: 'success' });
+
+			const currentProxySettings = getDevAssistCurrentSettings();
+			try {
+				const response = await fetch(`http://127.0.0.1:$${currentProxySettings.localPort}/api/internal/devassist`, {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: webviewMessage.data
+				});
+				if (response.ok) {
+					feedbackFormPanel?.webview.postMessage({ type: 'status', value: 'success' });
+				} else {
+					feedbackFormPanel?.webview.postMessage({ type: 'status', value: 'error' });
+				}
+			} catch (e) {
+				feedbackFormPanel?.webview.postMessage({ type: 'status', value: 'error' });
+			}
+			// Handle/store feedback as needed
+			// feedbackFormPanel?.dispose();
+			break;
+
+
+		case WEBVIEW_EVENTS.CLOSE:
+			feedbackFormPanel?.dispose();
+			break;
+	}
+}
 
 const getWebviewHTMLContent = (htmlFilePath : string, cssFilePath : string): string  => {
 	let htmlFileContent = FileUtils.readAsString(htmlFilePath);
