b076caa2e70431ca67e63f90a3a892b85dec9e8b
gerald.gillespie <gerald.gillespie@fullscript.com>
2026-02-28 11:02:38 -0500

    use sdf.config.js in the templater. better jsconfig template

--- packages/node-cli/src/templates/TemplateKeys.js
+++ packages/node-cli/src/templates/TemplateKeys.js
@@ -2,6 +2,8 @@
 ** Copyright (c) 2024 Oracle and/or its affiliates.  All rights reserved.
 ** Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 */
+const PACKAGE = require('../../package.json');
+const [firstConfig] = Array.isArray(PACKAGE.configFile) ? PACKAGE.configFile : ['sdf.config.js'];
 'use strict';
 
 module.exports = {
@@ -12,11 +14,11 @@ module.exports = {
 		commerceextension: require.resolve('./objects/commerceextension.xml'),
 	},
 	PROJECTCONFIGS: {
-		cliconfig: require.resolve('./projectconfigs/suitecloud.config.js'),
+		cliconfig: require.resolve(`./projectconfigs/${firstConfig}`),
 		gitignore: require.resolve('./projectconfigs/default_gitignore.template')
 	},
 	UNIT_TEST: {
-		cliconfig: require.resolve('./unittest/suitecloud.config.js.template'),
+		cliconfig: require.resolve(`./unittest/${firstConfig}.template`),
 		jestconfig: require.resolve('./unittest/jest.config.js.template'),
 		packagejson: require.resolve('./unittest/package.json.template'),
 		sampletest: require.resolve('./unittest/sample-test.js.template'),
